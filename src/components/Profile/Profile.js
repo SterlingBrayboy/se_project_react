b91b9706062ae0cc80aca2c88f15c7d0642e4382
LEARNER <sterlingbrayboyn@gmail.com>
2024-11-13 19:34:05 -0500

    Connecting Props

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,16 +3,21 @@ import ClothesSection from "../ClothesSection/ClothesSection";
 import SideBar from "../SideBar/SideBar";
 import "./Profile.css";
 
-const Profile = ({ clothingItems, onSelectCard, onCreateModal }) => {
+const Profile = ({
+  clothingItems,
+  onSelectCard,
+  onCreateModal,
+  handleEditModal,
+}) => {
   return (
     <div className="profile">
-      <SideBar />
+      <SideBar onEditClick={handleEditModal} />
       <div className="profile__items">
         <p>Your Items</p>
         <button
           type="button"
           className="profile__add-button"
-          onEditClick={onCreateModal}
+          onClick={onCreateModal}
         >
           + Add new
         </button>
