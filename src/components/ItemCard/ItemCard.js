998044e01935165616d4f773d9628f4bde6829e5
LEARNER <sterlingbrayboyn@gmail.com>
2025-01-09 17:18:18 -0500

    Fixing selectedCard

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -2,22 +2,29 @@ import "./ItemCard.css";
 import React, { useContext } from "react";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const ItemCard = ({ item, onSelectCard, onCardLike }) => {
+const ItemCard = ({ item, onSelectCard, onCardLike, selectedCard }) => {
   const { currentUser } = useContext(CurrentUserContext);
   console.log(item);
-  // const isOwn = selectedCard.owner === currentUser._id;
+  const isOwn = selectedCard.owner === currentUser._id;
   const isLiked = item.likes.some((like) => like === currentUser?._id);
 
   // const cardLikeVisible = `card__like ${
-  //   isOwn ? "card__like_visible" : "card__like_hidden"
+  // isOwn ? "card__like_visible" : "card__like_hidden"
   // }`;
 
-  const cardLiked = `card__like ${isLiked ? "card__like" : "card__unlike"}`;
+  // const cardLiked = `card__like ${isLiked ? "card__like" : "card__unlike"}`;
+
+  // Determine visibility of the like button based on ownership
+  const cardLikeVisible = isOwn ? "card__like_visible" : "card__like_hidden";
+
+  // Determine the style of the like button based on whether it's liked
+  const cardLiked = isLiked ? "card__like" : "card__unlike";
 
   return (
     <div className="card__area">
       <button
-        className={cardLiked}
+        // className={cardLiked}
+        className={`${cardLikeVisible} ${cardLiked}`}
         onClick={() => onCardLike(item, isLiked)}
       ></button>
       <img
